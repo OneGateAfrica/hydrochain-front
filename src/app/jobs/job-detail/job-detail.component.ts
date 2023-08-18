ddb5d2fece9e0be5eb660a0633ce62b5c77824ce
amine <medamine.riahi@esprit.tn>
2023-08-18 11:35:58 +0100

    commit 10

--- src/app/jobs/job-detail/job-detail.component.ts
+++ src/app/jobs/job-detail/job-detail.component.ts
@@ -91,7 +91,6 @@ export class JobDetailComponent implements OnInit{
     this.jobService.getById(this.id).subscribe((e :any) =>  {this.job = e.data.attributes ; console.log(this.job)});
   }
 
-  abcde
 
   applyAndClose() {
     this.modalService.dismissAll();
